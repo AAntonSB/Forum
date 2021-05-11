34d1048ca032fb43fa2226f71da542fbe59d35c3
AAntonSB <anton.s.bergqvist@gmail.com>
2021-05-11 19:07:02 +0200

    added mongodb

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const mongoose = require("mongoose")
+
+const db = require("./db");
+
 const app = express();
 const apiPort = 3000;
 
@@ -10,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(helmet());
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
