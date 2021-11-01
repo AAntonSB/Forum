e0ceb94b20cfe7e902d2cd8c876aea1535559393
AAntonSB <anton.s.bergqvist@gmail.com>
2021-11-01 12:17:11 +0100

    user route / model update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/user-model");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const { roleAuthMethods } = require("../helperFunctions");
