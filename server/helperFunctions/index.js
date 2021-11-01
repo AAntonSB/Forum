b269141fb29cb22bbe8ad3caa6398ca66242514b
AAntonSB <anton.s.bergqvist@gmail.com>
2021-11-01 12:17:24 +0100

    enum to helper funcs

--- server/helperFunctions/index.js
+++ server/helperFunctions/index.js
@@ -4,4 +4,11 @@ module.exports = {
   genUUID: function () {
     return v4();
   },
+  roleAuthMethods: {
+    SUPER_ADMIN: "superadmin",
+    ADMIN: "admin",
+    MATCH: "match",
+    MATCH_AND_APPLY: "matchAndApply",
+    GET_ROLES: "getroles",
+  },
 };
