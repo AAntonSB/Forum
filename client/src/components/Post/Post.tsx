e437c03adc01cd8b8a0c0582a279152fb9a34dbd
AAntonSB <anton.s.bergqvist@gmail.com>
2021-11-01 12:21:50 +0100

    Post updates

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { makeStyles } from '@material-ui/styles'
 import { ListItem, Typography } from '@material-ui/core'
 import { useParams } from 'react-router-dom'
+import chainBackground from '../../images/fence.png'
 import _ from 'lodash'
 
 const useStyles = makeStyles(() => ({
@@ -9,17 +10,32 @@ const useStyles = makeStyles(() => ({
     minHeight: "20rem",
     width: "100%",
     backgroundColor: "white",
-    marginTop: "3rem"
+    marginTop: "3rem",  
+    display: "grid",
+    gridTemplateRows: "10% auto 10%",
+    gridTemplateColumns: "100%"
+  },
+  lockedContainer: {
+    backgroundImage: `url(${chainBackground})`,
+    backgroundSize: "30%",
+    minHeight: "20rem",
+    width: "100%",
+    marginTop: "3rem",  
+    display: "grid",
+    gridTemplateRows: "10% auto 10%",
+    gridTemplateColumns: "100%"
   }
 }))
 
-const Post: FC = () => {
+const Post: FC<{ post: any }> = ({ post }) => {
   const classes = useStyles();
   const { name, id } = useParams<{ name: string, id: string }>();
 
   return (
-    <ListItem className={classes.mainContainer}>
-      
+    <ListItem className={post.locked?classes.lockedContainer:classes.mainContainer}>
+      <Typography variant="h5">{post.title}</Typography>
+      <Typography style={{ marginTop: "1%", alignSelf: "flex-start" }}>{post.text}</Typography>
+      <Typography>{post.user}</Typography>
     </ListItem>
   )
 }
--- client/src/components/Post/PostReply.tsx
+++ client/src/components/Post/PostReply.tsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles(() => ({
 
 const PostReply: FC = () => {
   const classes = useStyles();
-  const { name, id } = useParams<{ name: string, id: string }>();
+  
 
   return (
     <div>
