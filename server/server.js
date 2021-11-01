d31c7a49dcc68b362cde5c3d9e8ea0eda2dcc87b
AAntonSB <anton.s.bergqvist@gmail.com>
2021-11-01 12:18:20 +0100

    Server updates

--- server/server.js
+++ server/server.js
@@ -4,45 +4,66 @@ const cors = require("cors");
 const helmet = require("helmet");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
 
-const db = require("./db");
-const userRouter = require("./routes/user-router");
-const subForumRouter = require("./routes/sub-forum-router");
-const postRouter = require("./routes/post-router");
+if (process.env.NODE_ENV !== "production") {
+  // Load environment variables from .env file in non prod environments
+  require("dotenv").config();
+}
+require("./db");
+
+require("./strategies/JwtStrategy");
+require("./strategies/LocalStrategy");
+require("./authenticate");
+
+const userRouter = require("./routes/userRoutes");
+const roleRouter = require("./routes/roleRoutes");
+const subForumRouter = require("./routes/subForumRoutes");
+const postRouter = require("./routes/postRouter");
+const replyRouter = require("./routes/replyRouter");
+
+// const userRouter = require("./routes1/user-router");
+// const subForumRouter = require("./routes1/sub-forum-router");
+// const postRouter = require("./routes1/post-router");
 
 const app = express();
 const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:8000",
-    credentials: true,
-  })
-);
 app.use(bodyParser.json());
-app.use(helmet());
-app.use(
-  session({
-    secret: "secretCode",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-app.use(cookieParser("secretCode"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//Add the client URL to the CORS policy
+const whitelist = process.env.WHITELISTED_DOMAINS
+  ? process.env.WHITELISTED_DOMAINS.split(",")
+  : [];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(passport.initialize());
-app.use(passport.session());
-require("./passportConfig")(passport);
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+app.use("/users", userRouter);
+app.use("/roles", roleRouter);
+app.use("/subForums", subForumRouter);
+app.use("/posts", postRouter);
+app.use("/replies", replyRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.use(helmet());
+
+app.get("/", function (req, res) {
+  res.send({ status: "success" });
 });
 
-app.use("/api", userRouter);
-app.use("/api", subForumRouter);
-app.use("/api", postRouter);
-
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+const server = app.listen(process.env.PORT || 3000, function () {
+  const port = server.address().port;
+  console.log("App started at port:", port);
+});
